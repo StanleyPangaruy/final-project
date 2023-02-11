d388259b2d609a76028d4b43fd45e76693a6c0f0
unknown <pangaruystanley@gmail.com>
2023-02-11 20:20:39 +0800

    change cities into towns from Quezon Province 3rd and 4th district and added their distances on the roads from each other

--- project.py
+++ project.py
@@ -3,29 +3,53 @@ import networkx as nx
 # Create a Graph
 G = nx.Graph()
 
-# Add cities as nodes in the graph
-cities = ["Delhi", "Mumbai", "Kolkata", "Chennai", "Bangalore"]
-for city in cities:
-    G.add_node(city)
+# Add towns as nodes in the graph
+towns = ["Agdangan", "Buenavista", "Catanauan", "General Luna", "Macalelon", "Mulanay", "Padre Burgos",
+          "Pitogo", "San Andres", "San Francisco", "San Narciso", "Unisan", "Alabat", "Atimonan", "Calauag",
+          "Guinayangan", "Gumaca", "Lopez", "Perez", "Plaridel", "Quezon", "Tagkawayan"]
+for town in towns:
+    G.add_node(town)
 
-# Add edges between the cities with a weight representing the distance between the cities
-G.add_edge("Delhi", "Mumbai", weight=1200)
-G.add_edge("Delhi", "Kolkata", weight=800)
-G.add_edge("Delhi", "Chennai", weight=1100)
-G.add_edge("Mumbai", "Kolkata", weight=900)
-G.add_edge("Mumbai", "Bangalore", weight=500)
-G.add_edge("Kolkata", "Chennai", weight=600)
-G.add_edge("Chennai", "Bangalore", weight=300)
+# Add edges between the towns with a weight representing the distance between the towns
+G.add_edge("Atimonan", "Plaridel", weight=11.8)
+G.add_edge("Atimonan","Unisan", weight=27.4)
+G.add_edge("Atimonan","Padre Burgos", weight=39)
+G.add_edge("Plaridel","Unisan", weight=25.5)
+G.add_edge("Plaridel","Gumaca", weight=11.3)
+G.add_edge("Gumaca","Pitogo", weight=22.6)
+G.add_edge("Gumaca","Quezon", weight=17.1)
+G.add_edge("Gumaca","Alabat", weight=40)
+G.add_edge("Gumaca","Perez", weight=55.6)
+G.add_edge("Gumaca","Lopez", weight=19.8)
+G.add_edge("Gumaca","Unisan", weight=33.4)
+G.add_edge("Lopez","Calauag", weight=10.6)
+G.add_edge("Lopez","Catanauan", weight=41.9)
+G.add_edge("Lopez","Buenavista", weight=33)
+G.add_edge("Calauag","Guinayangan", weight=25.7)
+G.add_edge("Calauag","Tagkawayan", weight=52.8)
+G.add_edge("Agdangan","Unisan", weight=9.3)
+G.add_edge("Padre Burgos","Agdangan", weight=23.5)
+G.add_edge("Unisan","Pitogo", weight=21.2)
+G.add_edge("Pitogo","Macalelon", weight=24.2)
+G.add_edge("Macalelon","General Luna", weight=20.8)
+G.add_edge("General Luna","Catanauan", weight=26)
+G.add_edge("Catanauan","Mulanay", weight=23.7)
+G.add_edge("San Francisco","Mulanay", weight=25.4)
+G.add_edge("San Andres","San Francisco", weight=18.1)
+G.add_edge("San Narciso","San Andres", weight=40.1)
+G.add_edge("Buenavista","San Narciso", weight=27.6)
+G.add_edge("Buenavista","Catanauan", weight=24.9)
+G.add_edge("San Narciso","Mulanay", weight=39.1)
 
-# Find the shortest path between two cities using Dijkstra's Algorithm
+# Find the shortest path between two towns using Dijkstra's Algorithm
 def find_shortest_path(G, start, end):
     return nx.dijkstra_path(G, start, end, weight='weight')
 
-# Input the start and end cities from the user
-start_city = input("Enter start city: ")
-end_city = input("Enter end city: ")
+# Input the start and end towns from the user
+start_city = input("Enter starting town: ")
+end_city = input("Enter ending town: ")
 
-# Get the shortest path between the two cities
+# Get the shortest path between the two towns
 shortest_path = find_shortest_path(G, start_city, end_city)
 
 # Print the shortest path
